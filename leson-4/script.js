67ec47c0781b773439c4604d76fe595eb484c976
avo2003 <avetis.f.y@tumo.org>
2019-05-11 12:19:34 +0400

    prog 3

---
+++ leson-4/script.js
@@ -0,0 +1,102 @@
+// var clickCount = 0;
+// function clickHandler(evt){
+//    clickCount++;
+//    console.log(evt);
+//    var str = "Thanks for clicking " + clickCount;
+//    this.innerText = str;
+// }
+
+// var p = document.getElementById("pElement");
+// p.addEventListener("click", clickHandler);
+
+// function bodyClick(evt) {
+//     console.log(evt.pageX, evt.pageY);
+//  }
+//  window.onclick = bodyClick;
+ 
+
+
+
+// function bodyClick(evt) {
+//     console.log(evt.pageX, evt.pageY);
+//  }
+//  window.onclick = bodyClick;
+ 
+
+
+
+// function setup() {
+    
+// }
+// function mouseClicked() {
+//     console.log(mouseX, mouseY);
+//  }
+//  // setup(), draw() ...
+ 
+
+
+// function windowLoad() {
+//     console.log("Window is loaded");
+//  }
+//  window.onload = windowLoad;
+//  // .addEventListener("onload",windowLoad)
+
+ 
+
+
+// function setup() {
+    
+// }
+//  function preload() {
+//     console.log("Window is loaded");
+//  }
+ 
+//  // setup(), draw() ...
+
+
+
+
+
+
+
+// function click(evt) {
+//     console.log(evt.pageX, evt.pageY);
+//  }
+//  window.onclick = click;
+//  // .addEventListener("click", click);
+ 
+ 
+
+// function setup() {
+    
+// }
+// function mousePressed() {
+//     console.log(mouseX, mouseY);
+//  }
+ 
+//  // setup(), draw() ...
+
+
+
+
+
+// function keydown(evt) {
+//     console.log("You printed " + evt.key);
+//  }
+//  window.onkeydown = keydown;
+//  // .addEventListener('keydown', keydown);
+
+ 
+
+
+
+// function setup() {
+    
+// }
+//  function keyPressed() {
+//     console.log(key);
+//  }
+ 
+//  // setup(), draw() ...
+ 
+ 
